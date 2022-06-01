e500ef1d0d550f282abf32dfb4226a41be0380db
AtomLabX <olmpy@hotmail.com>
2022-06-01 00:03:47 -0600

    Modfied the text and added a note for the future to look back on, also, I cleaned the code a bit

--- src/ChatBot_V1.java
+++ src/ChatBot_V1.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ChatBot_V1 {
     public static void main(String[] args) {
+        // Note: Always add new Scanner then (System.in) for input
         Scanner input = new Scanner(System.in);
         System.out.println("Hello, I'm your chat bot, let's start by setting me up");
         System.out.print("What you would like to name me? ");
@@ -10,9 +11,11 @@ public class ChatBot_V1 {
         System.out.print("What is your name please? ");
         String name = input.nextLine();
         System.out.println("very nice name, "+name);
-        System.out.println("Heads up and look above!, You are not allowd to use me if you are under the age of 16 years old!");
+        System.out.println("Heads up and look above!, You are not allowd to use me if you are " +
+                "under the age of 16 years old!");
         System.out.print("Is that clear? (Yes or No?) ");
         String answer = input.nextLine();
-        System.out.println("That is it for now, see you tomorrow for the rest of the setup, great things awaits! I tell ya, HAHA!");
+        System.out.println("That is it for now, see you tomorrow for the rest of the setup, " +
+                "great things awaits! I tell ya, HAHA!");
     }
 }
